efc466cb28791197fa6dea5cacd8c91b1e3db61f
Jesus <jesusherreraeo@gmail.com>
2023-03-04 20:38:15 -0300

    agrega estilos y acomoda js

--- js/code.js
+++ js/code.js
@@ -5,39 +5,9 @@ button.addEventListener("click", () => {
     button = prompt("Ingrese su edad");
 
     if (button >= "18"){
-        location.host=127.0.0.1:5500/html/inicio.html;
+        window.location="./html/inicio.html"
     } else {
         alert("Por favor no comparta esta informacion, entrará bajo su propio riesgo");
-        location.host=127.0.0.1:5500/html/inicio.html;
+        window.location="./html/inicio.html"
     }
 });
-
-
-p id= location
-//PROPIEDADES DEL OBJETO LOCATION
-var texto =
-//href: HREF (URL) de la padgina
-
-texto += "<br/>Href: " +location.href;
-//hostname: nombre del host de la pagina
-texto += "<br/>Hostname: " +location. hostname;
-//pathname: pathname de la pdgina
-
-texto += "<br/>Pathname: " +location. pathname;
-//protocol: protocolo de la pagina
-
-texto += "<br/>Protocol: " +location. protocol;
-//hash: hash o ancla de la pdgina (Ej. www.web.com/index. html#indice)
-
-texto += "<br/>Hash: " +location. hasg;
-
- 
-//bost: nombre del hostname y el puerto
-texto += "<br/>Host: " +location. host;
-//origin: nombre del protocolo, hostname y el puerto
-
-texto += "<br/>0rigin: " +location.origin;
-
-//search: querystring de la pagina (Ej. www.web.com/index.html?user=ada)
-texto += "<br/>Search: " +location.search;    
-document.getElementById("location").innerHTML = texto;
